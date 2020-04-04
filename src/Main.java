d0420c345921f0ac04e9b9d9c7954a3aa791b800
AhmedGamalWazeiry <ahmedgamal1452@gmail.com>
2020-04-04 22:49:06 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+
+public class Main {
+    public static  void main(String[] args) throws IOException{
+        String srcFile = "good.cl";
+        if(args.length >= 1)
+            srcFile =args[0];
+        Lexer_IO lexer = new Lexer_IO(srcFile);
+        lexer.writeTokens(srcFile +"-lex");
+    }
+}
